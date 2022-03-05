63ea0a147080ad7ab6531bc5e679d996b674d2a9
Erick Oliveira <erickoliveirasystem@outlook.com>
2022-03-05 10:30:56 -0300

    Add Table to NavBar

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -15,14 +15,24 @@ import {
   PopoverHeader,
   PopoverTrigger,
   Spacer,
+  Table,
+  Tbody,
+  Td,
   Text,
+  Th,
+  Thead,
   Tooltip,
+  Tr,
 } from "@chakra-ui/react";
 import { TimeIcon } from "@chakra-ui/icons";
 import { useProfile } from "context/Profile";
 
 const NavBar = () => {
-  const { hoursCompleted } = useProfile();
+  const {
+    hoursCompleted,
+    isComplementaryActivitiesTaken,
+    isRequiredInternshipTaken,
+  } = useProfile();
 
   return (
     <Flex justify="end" w="100%" p="5px 5px 0px 0px">
@@ -35,11 +45,32 @@ const NavBar = () => {
           </Box>
         </Tooltip>
         <MenuList p="5px 10px">
-          <Heading as="h4" size="sm" padding={"4px 0px 7px 0px"}>
-            Carga Horária para Integralização
-          </Heading>
-          <Divider m="2px" />
-          Horas Integralizada: {hoursCompleted} / 3.690
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>Carga Horária para Integralização</Th>
+                <Th isNumeric>Integralizada</Th>
+                <Th isNumeric>Exigida</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td>Obrigatórias</Td>
+                <Td isNumeric>{hoursCompleted}</Td>
+                <Td isNumeric>3.690</Td>
+              </Tr>
+              <Tr>
+                <Td>Atividades Complementares</Td>
+                <Td isNumeric>{isComplementaryActivitiesTaken ? 30 : 0}</Td>
+                <Td isNumeric>30</Td>
+              </Tr>
+              <Tr>
+                <Td>Estágio Obrigatório</Td>
+                <Td isNumeric>{isRequiredInternshipTaken ? 180 : 0}</Td>
+                <Td isNumeric>180</Td>
+              </Tr>
+            </Tbody>
+          </Table>
         </MenuList>
       </Menu>
     </Flex>
