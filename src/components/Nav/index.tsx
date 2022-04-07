3a3490d0008c42d9d845402ef58879201b375d59
Erick Oliveira <erick.oliveira@stage.consulting>
2022-04-07 17:32:35 -0300

    Fix: Menu styling

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -26,6 +26,7 @@ import {
 } from "@chakra-ui/react";
 import { TimeIcon } from "@chakra-ui/icons";
 import { useProfile } from "context/Profile";
+import { StyledMenuList } from "./styled";
 
 const NavBar = () => {
   const {
@@ -44,30 +45,34 @@ const NavBar = () => {
             </MenuButton>
           </Box>
         </Tooltip>
-        <MenuList p="5px 10px" maxW={"100vw"}>
+        <StyledMenuList>
           <Table variant="simple">
             <Thead>
               <Tr>
                 <Th>Carga Horária para Integralização</Th>
-                <Th isNumeric>Integralizada/Exigida</Th>
+                <Th isNumeric>Integralizada</Th>
+                <Th isNumeric>Exigida</Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
                 <Td>Obrigatórias</Td>
-                <Td isNumeric>{hoursCompleted}/3.690</Td>
+                <Td isNumeric>{hoursCompleted}</Td>
+                <Td isNumeric>3.690</Td>
               </Tr>
               <Tr>
                 <Td>Atividades Complementares</Td>
-                <Td isNumeric>{isComplementaryActivitiesTaken ? 30 : 0}/30</Td>
+                <Td isNumeric>{isComplementaryActivitiesTaken ? 30 : 0}</Td>
+                <Td isNumeric>30</Td>
               </Tr>
               <Tr>
                 <Td>Estágio Obrigatório</Td>
-                <Td isNumeric>{isRequiredInternshipTaken ? 180 : 0}/180</Td>
+                <Td isNumeric>{isRequiredInternshipTaken ? 180 : 0}</Td>
+                <Td isNumeric>180</Td>
               </Tr>
             </Tbody>
           </Table>
-        </MenuList>
+        </StyledMenuList>
       </Menu>
     </Flex>
   );
